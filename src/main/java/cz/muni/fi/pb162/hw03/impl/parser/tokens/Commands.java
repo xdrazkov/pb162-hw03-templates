1a8f0a573987187b70cf43bd22077d829405e667
jcechace <jcechace@gmail.com>
2023-04-26 20:41:49 +0200

    Adding command constants

---
+++ src/main/java/cz/muni/fi/pb162/hw03/impl/parser/tokens/Commands.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pb162.hw03.impl.parser.tokens;
+
+/**
+ * Command name constants
+ */
+public final class Commands {
+
+    public static final String IF = "if";
+    public static final String ELSE = "else";
+    public static final String FOR = "for";
+    public static final String DONE = "done";
+
+    private Commands() {
+        // intentionally private
+    }
+}
